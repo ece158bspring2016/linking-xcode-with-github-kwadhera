e4596227ad4f2a2b24dd130fb75c334205eb3827
kwadhera <m4neokw@gmail.com>
2016-03-31 11:27:34 -0700

    Initial Commit

---
+++ Link Xcode to GithubTests/Link_Xcode_to_GithubTests.swift
@@ -0,0 +1,36 @@
+//
+//  Link_Xcode_to_GithubTests.swift
+//  Link Xcode to GithubTests
+//
+//  Created by Karan Wadhera on 3/31/16.
+//  Copyright © 2016 Karan Wadhera. All rights reserved.
+//
+
+import XCTest
+@testable import Link_Xcode_to_Github
+
+class Link_Xcode_to_GithubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
